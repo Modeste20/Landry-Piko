fc7386143b810e3841a6a7b67fe7f72743be7c99
Modeste Landry <jojomodeste52@gmail.com>
2022-05-29 12:15:50 +0200

    Ajout de la section de témoignages et de tarifs

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 
 
-
+/* Handle open/close navbar on mobile */
 const barsElment = document.getElementById('header-bars');
 
 const openHeaderMenu = (e) => {
@@ -9,3 +9,34 @@ const openHeaderMenu = (e) => {
 }
 
 barsElment.addEventListener('click',openHeaderMenu)
+
+
+/*
+    Toogle is-active class when links on header is clicked
+*/
+
+const headerLinks = document.querySelectorAll('.header .header-navbar ul li a')
+
+for(const i of headerLinks){
+    i.addEventListener('click',(e) => {
+        if(e.currentTarget.classList.contains('is-active')){
+            e.preventDefault()
+        } else{
+            document.querySelector('.header .header-navbar ul li a.is-active').classList.remove('is-active')
+            e.currentTarget.classList.add('is-active')
+        }
+    })
+}
+
+/*
+    Automatiquement activer le lien de l'ancre
+*/
+const {hash} = window.location;
+
+if(hash){
+    const linkElement = document.querySelector('.header .header-navbar ul li a'+hash)
+    if(linkElement){
+        document.querySelector('.header .header-navbar ul li a.is-active').classList.remove('is-active')
+        linkElement.classList.add('is-active')
+    }
+}
